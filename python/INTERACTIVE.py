356cc960044e72e4c17e6b339b5e94dce5800c9c
eyvindson <31066846+eyvindson@users.noreply.github.com>
2023-07-04 14:45:59 +0200

    Update INTERACTIVE.py
    Updating the figure -- many changes.

--- python/INTERACTIVE.py
+++ python/INTERACTIVE.py
@@ -1,20 +1,63 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import ipywidgets as widgets
-from IPython.display import display, clear_output
+def show_Period_plot_interactive(MM, SUVT, plotts, EXTRA):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import pandas as pd
+    from scipy.stats import gaussian_kde, scoreatpercentile
+    import warnings
 
-#Periods
-def show_Period_plot_interactive(MM,SUVT,plotts):
+    AREA = 287.5
+    path = "./"
+    files, rs = "SP_RESULTS_", "results/"
+    var = ['Low_Low', 'High_Low', 'Mod_Low', 'Mod_Mid', 'Low_High']
+    fig, axs = plt.subplots(6, 2, figsize=(18, 18), dpi=100)
+
+    for SUV in SUVT:
+        ls = "solid" if SUV == "_SALVAGE_NPV" else "--"
+        colors = ["red", "blue", "green", "black", "orange"]
+
+        for i, lm in enumerate(plotts):
+            t11 = pd.read_csv(path + rs + files + "unsolved_EF_INCOME_" + var[lm] + SUV + EXTRA + ".csv")
+            t11['EF_YEAR'] = t11['EF_YEAR'].str[0:4]
+            YR = [str(2016 + 5 * i) for i in range(0, 6)]
+            ax = axs[i // 2, i % 2]
+
+            for j in range(6):
+                data = t11.set_index("EF_YEAR").loc[YR[j]]["EF_AVG_NPV_unsolved_" + MM] / AREA
+                data.plot.kde(alpha=0.5, ax=ax, color=colors[lm], linestyle=ls)
+
+                xmin, xmax = ax.get_xlim()
+                cvar_5_11 = scoreatpercentile(data, 5, interpolation_method='lower')
+
+                ax.plot([cvar_5_11], [0], marker="^", color=colors[lm], markeredgecolor="black", markersize=6)
+
+                kde = gaussian_kde(data)
+                x = np.linspace(xmin, xmax, 1000)
+                kde_y = kde(x)
+                x0 = x[x < 200000 / AREA]
+                x0_len = len(x0)
+                y0 = kde_y[:x0_len]
+
+                if x0_len > 1:
+                    ax.fill_between(x=x0, y1=y0, color=colors[lm], alpha=.5)
+
+            #ax.get_legend().remove()
+            ax.set_title("Period {} Income".format(i + 1))
+
+    plt.show()
+
+    
+def show_Period_plot_interactive(MM,SUVT,plotts,EXTRA):
     from matplotlib.lines import Line2D
     import numpy as np
     import pandas as pd
     import matplotlib.pyplot as plt
+    from scipy.stats import gaussian_kde, scoreatpercentile
     import warnings
     warnings.filterwarnings("ignore", message="Auto-removal of overlapping axes is deprecated")
 
     path = "./"
     files, rs =  "SP_RESULTS_", "results/"
-
+    AREA = 287.5
     var = ['Low_Low',   'High_Low', 'Mod_Low','Mod_Mid','Low_High']
     fig, ax = plt.subplots(figsize=(18, 18), dpi=100)
     ax1 = plt.subplot2grid((5, 2), (1, 0), colspan=1)
@@ -31,18 +74,20 @@ def show_Period_plot_interactive(MM,SUVT,plotts):
             ls = "--"
         colors = ["red","blue","green","black","orange"]
         for lm in plotts:
-            t11 = pd.read_csv(path+rs+files+"unsolved_EF_INCOME_"+var[lm]+SUV+".csv")
+            t11 = pd.read_csv(path+rs+files+"unsolved_EF_INCOME_"+var[lm]+SUV+EXTRA+".csv")
             t11['EF_YEAR']=t11['EF_YEAR'].str[0:4]
             YR= [str(2016 + 5*i) for i in range(0,6)]
             t = [ax1,ax2,ax3,ax4,ax5,ax6]
+            check = []
+            t1 = pd.read_csv(path+rs+files+"unsolved_EF_"+var[lm]+SUV+EXTRA+".csv")
             for i in range(0,6):
-                t11.set_index("EF_YEAR").loc[YR[i]][["EF_AVG_NPV_unsolved_"+MM]].plot.kde(alpha = 0.5,ax=t[i],color=colors[lm],linestyle=ls)
+                (t11.set_index("EF_YEAR").loc[YR[i]][["EF_AVG_NPV_unsolved_"+MM]]/AREA).plot.kde(alpha = 0.5,ax=t[i],color=colors[lm],linestyle=ls)
                 xmin1, xmax1 = t[i].get_xlim()
-                cvar_5_11 = scoreatpercentile(t11.set_index("EF_YEAR").loc[YR[i]][["EF_AVG_NPV_unsolved_"+MM]], 5, interpolation_method='lower')
+                cvar_5_11 = scoreatpercentile(t11.set_index("EF_YEAR").loc[YR[i]][["EF_AVG_NPV_unsolved_"+MM]]/AREA, 5, interpolation_method='lower')
 
                 t[i].plot([cvar_5_11],[0], marker = "^", color=colors[lm],markeredgecolor= "black",markersize=6)
 
-                kde = gaussian_kde(t11.set_index("EF_YEAR").loc[YR[i]][["EF_AVG_NPV_unsolved_"+MM]]["EF_AVG_NPV_unsolved_"+MM])
+                kde = gaussian_kde(t11.set_index("EF_YEAR").loc[YR[i]][["EF_AVG_NPV_unsolved_"+MM]]["EF_AVG_NPV_unsolved_"+MM]/AREA)
                 xmin, xmax= xmin1,xmax1
 
                 # create points between the min and max
@@ -52,7 +97,7 @@ def show_Period_plot_interactive(MM,SUVT,plotts):
                 kde_y = kde(x)
 
                 # select x values below 0
-                x0 = x[x < 200000]
+                x0 = x[x < 200000/AREA]
 
                 # get the len, which will be used for slicing the other arrays
                 x0_len = len(x0)
@@ -65,118 +110,121 @@ def show_Period_plot_interactive(MM,SUVT,plotts):
                 # fill the areas
                 if x0_len>1:
                     t[i].fill_between(x=x0, y1=y0, color=colors[lm], alpha=.5)
+                check = check+[cvar_5_11]
             titles =["1st Period Income","2nd Period Income","3rd Period Income","4th Period Income","5th Period Income","6th Period Income"]
             for i in range(0,6):
                 t[i].get_legend().remove()
                 t[i].set_title(titles[i])
+            
+                
     
     plt.show()
+    
 
-#NPV
-def show_NPV_plot_interactive(MM,SUVT,plotts):
-    from matplotlib.lines import Line2D
-    import numpy as np
-    import pandas as pd
+def show_NPV_plot_interactive(MM, SUVT, plotts, EXTRA):
     import matplotlib.pyplot as plt
-    path = "./"
-    files, rs, VARI =  "SP_RESULTS_", "results/", "MAX_AVG_NPV_unsolved_"
+    import pandas as pd
 
-    var = ['Low_Low',   'High_Low', 'Mod_Low','Mod_Mid','Low_High']
+    AREA = 287.5
+    path = "./"
+    files, rs, VARI = "SP_RESULTS_", "results/", "MAX_AVG_NPV_unsolved_"
+    var = ['Low_Low', 'High_Low', 'Mod_Low', 'Mod_Mid', 'Low_High']
     fig, ax = plt.subplots(figsize=(6, 6), dpi=100)
+
     for SUV in SUVT:
-        if SUV == "_SALVAGE_NPV":
-            ls = "solid"
-        else:
-            ls = "--"
-        colors = ["red","blue","green","black","orange"]
+        ls = "solid" if SUV == "_SALVAGE_NPV" else "--"
+        colors = ["red", "blue", "green", "black", "orange"]
 
         for lm in plotts:
-            t1 = pd.read_csv(path+rs+files+"unsolved_EF_"+var[lm]+SUV+".csv")
-            t1[VARI+MM].plot.kde(ax= ax,alpha = 0.5,color = colors[lm],linestyle=ls)
+            t1 = pd.read_csv(path + rs + files + "unsolved_EF_" + var[lm] + SUV + EXTRA + ".csv")
+            (t1[VARI + MM] / AREA).plot.kde(ax=ax, alpha=0.5, color=colors[lm], linestyle=ls)
             ax.set_title("Net Present Value")
-            custom_lines = [(Line2D([0], [0], color=colors[i], lw=4)) for i in plotts]
-            ax.plot([t1[VARI+MM].mean()],[0], marker = "x", color=colors[lm])
-            ax.legend(custom_lines, [var[i] for i in plotts],bbox_to_anchor=(1.3, 1.0))
+            custom_lines = [plt.Line2D([0], [0], color=colors[i], lw=4) for i in plotts]
+            ax.plot([t1[VARI + MM].mean() / AREA], [0], marker="x", color=colors[lm])
+            ax.legend(custom_lines, [var[i] for i in plotts], bbox_to_anchor=(1.3, 1.0))
+
     ax.set_ylim(bottom=0)
     plt.show()
 
-    
+
+Graph = show_NPV_plot_interactive
+
+
 def show_GUI():
-    Graph = show_NPV_plot_interactive
-    # Create radio buttons for variable selection
+    import ipywidgets as widgets
+    from IPython.display import display, clear_output
+
     var_select = widgets.RadioButtons(
-        options=["LL","HL","MM","ML","LH"],
+        options=["LL", "HL", "MM", "ML", "LH"],
         value='LL',
         description='Outcome scenario:',
-        disabled=False
+        disabled=False,
+        layout=widgets.Layout(width='150px')
     )
 
     graph_select = widgets.RadioButtons(
-        options=["Net Present Value","Periodic Income"],
+        options=["Net Present Value", "Periodic Income"],
         value='Net Present Value',
-        description='Which variable to graph:',
-        disabled=False
+        description='Which variable:',
+        disabled=False,
+        layout=widgets.Layout(width='150px')
     )
 
-    dictvar = {"LL":0,"HL":1,"MM":2,"ML":3,"LH":4}
-    
-    # Call the plot function when a new variable is selected
-    
-    def on_graph_change1(change):
-        #print(f'Selected variable: {change.new}')
+    salvage_select = widgets.RadioButtons(
+        options=["5", "10", "15", "20"],
+        value='5',
+        description='Which salvage price:',
+        disabled=False,
+        layout=widgets.Layout(width='150px')
+    )
+
+    dictvar = {"LL": 0, "HL": 1, "MM": 2, "ML": 3, "LH": 4}
+
+    checkboxes = [
+        widgets.Checkbox(description="LL", indent = False, value=True, layout=widgets.Layout(width='150px')),
+        widgets.Checkbox(description="HL", indent = False,layout=widgets.Layout(width='150px')),
+        widgets.Checkbox(description="MM", indent = False,layout=widgets.Layout(width='150px')),
+        widgets.Checkbox(description="ML", indent = False,layout=widgets.Layout(width='150px')),
+        widgets.Checkbox(description="LH", indent = False,value=True, layout=widgets.Layout(width='150px'))
+    ]
+
+    OBJboxes = [
+        widgets.Checkbox(description="NPV", indent = False,value=True, layout=widgets.Layout(width='150px')),
+        widgets.Checkbox(description="CVAR", indent = False,layout=widgets.Layout(width='150px'))
+    ]
+
+    def on_change(change):
         clear_output()
-        if change.new == "Net Present Value":
+        variables = [dictvar[i] for i in [checkbox.description for checkbox in checkboxes if checkbox.value]]
+        obj_variables = ["_SALVAGE_" + i for i in [checkbox.description for checkbox in OBJboxes if checkbox.value]]
+        display(widgets.HBox([graph_select, salvage_select, obj_checkboxes_container, var_select, checkboxes_container]))
+        print(graph_select.value)
+        global Graph
+        if graph_select.value == "Net Present Value":
             Graph = show_NPV_plot_interactive
         else:
             Graph = show_Period_plot_interactive
-        display(widgets.HBox([graph_select,widgets.VBox([OBJ_title, widgets.VBox(OBJboxes)]),var_select, widgets.VBox([checkboxes_title, widgets.VBox(checkboxes)])]))
-        Graph(var_select.value,["_SALVAGE_"+i for i in [checkbox.description for checkbox in OBJboxes if checkbox.value]],[dictvar[i] for i in [checkbox.description for checkbox in checkboxes if checkbox.value]])
+        Graph(var_select.value,obj_variables,variables,"_" + salvage_select.value + ".0")
+        
 
-    graph_select.observe(on_graph_change1, 'value')
-    
-    def on_var_change1(change):
-        #print(f'Selected variable: {change.new}')
-        clear_output()
-        display(widgets.HBox([graph_select,widgets.VBox([OBJ_title, widgets.VBox(OBJboxes)]),var_select, widgets.VBox([checkboxes_title, widgets.VBox(checkboxes)])]))
-        Graph(change.new,["_SALVAGE_"+i for i in [checkbox.description for checkbox in OBJboxes if checkbox.value]],[dictvar[i] for i in [checkbox.description for checkbox in checkboxes if checkbox.value]])
-
-    var_select.observe(on_var_change1, 'value')
-
-    # Create the checkboxes for variable selection
-    checkboxes = [widgets.Checkbox(description="LL",value = True), 
-                  widgets.Checkbox(description="HL"), 
-                  widgets.Checkbox(description="MM"),
-                  widgets.Checkbox(description="ML"),
-                  widgets.Checkbox(description="LH",value = True)]
-
-    OBJboxes = [widgets.Checkbox(description="NPV",value = True), 
-                  widgets.Checkbox(description="CVAR")]
-
-    # Define the callback function for the checkboxes
-    def on_checkbox_change(change):
-        clear_output()
-        variables = [dictvar[i] for i in [checkbox.description for checkbox in checkboxes if checkbox.value]]
-        display(widgets.HBox([graph_select,widgets.VBox([OBJ_title, widgets.VBox(OBJboxes)]),var_select, widgets.VBox([checkboxes_title, widgets.VBox(checkboxes)])]))
-        Graph(var_select.value,["_SALVAGE_"+i for i in [checkbox.description for checkbox in OBJboxes if checkbox.value]],variables)
-
-
-    def on_OBJbox_change(change):
-        clear_output()
-        variables = ["_SALVAGE_"+i for i in [checkbox.description for checkbox in OBJboxes if checkbox.value]]
-        display(widgets.HBox([graph_select,widgets.VBox([OBJ_title, widgets.VBox(OBJboxes)]),var_select, widgets.VBox([checkboxes_title, widgets.VBox(checkboxes)])]))
-        Graph(var_select.value,variables,[dictvar[i] for i in [checkbox.description for checkbox in checkboxes if checkbox.value]])
-
-    checkboxes_title = widgets.Label(value='Planned for scenarios:')
-    OBJ_title=widgets.Label(value='Objectives:')
-    # Attach the callback function to each checkbox
+    var_select.observe(on_change, 'value')
+    graph_select.observe(on_change, 'value')
+    salvage_select.observe(on_change, 'value')
     for checkbox in checkboxes:
-        checkbox.observe(on_checkbox_change, 'value')
-
+        checkbox.observe(on_change, 'value')
     for checkbox in OBJboxes:
-        checkbox.observe(on_OBJbox_change, 'value')    
+        checkbox.observe(on_change, 'value')
 
-    # Display the radio buttons, checkboxes and initial plot
-    display(widgets.HBox([graph_select,widgets.VBox([OBJ_title, widgets.VBox(OBJboxes)]),var_select, widgets.VBox([checkboxes_title, widgets.VBox(checkboxes)])]))
+    checkboxes_container = widgets.VBox([
+        widgets.Label(value='Planned for scenarios:'),
+        widgets.VBox(checkboxes, layout=widgets.Layout(justify_content ='flex-start'))
+    ])
 
-    #plot_func(var_select.value)
-    Graph(var_select.value,["_SALVAGE_"+i for i in [checkbox.description for checkbox in OBJboxes if checkbox.value]],[dictvar[i] for i in [checkbox.description for checkbox in checkboxes if checkbox.value]])
+    obj_checkboxes_container = widgets.VBox([
+        widgets.Label(value='Objectives:\r'),
+        widgets.VBox(OBJboxes, layout=widgets.Layout(justify_content ='flex-start'))
+    ])
+
+    display(widgets.HBox([graph_select, salvage_select, obj_checkboxes_container, var_select, checkboxes_container]))
+    Graph(var_select.value,
+          ["_SALVAGE_" + i for i in [checkbox.description for checkbox in OBJboxes if checkbox.value]], [dictvar[i] for i in [checkbox.description for checkbox in checkboxes if checkbox.value]], "_" + salvage_select.value + ".0")
